5bd147c0c2a51a0f9d314323aad8ccfeec0cc2df
iftekher2108 <parvezkhan21082002@gmail.com>
2025-07-16 22:56:00 +0600

    model component added

--- resources/views/backend/catagory/index.blade.php
+++ resources/views/backend/catagory/index.blade.php
@@ -1,111 +1,115 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+    <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
         <div>
             <h3 class="fw-bold mb-3">Category</h3>
             {{-- <h6 class="op-7 mb-2">Admin Dashboard</h6> --}}
         </div>
         <div class="ms-md-auto py-2 py-md-0">
             {{-- <a href="#" class="btn btn-label-info btn-round me-2">Refresh</a> --}}
-            <a href="#" class="btn btn-primary btn-round">Add Category</a>
+            <x-model.m-button label='Add Category' />
         </div>
     </div>
 
 
-      <div class="col-md-12">
-              <div class="card">
-                <div class="card-header">
-                  {{-- <h4 class="card-title">Multi Filter Select</h4> --}}
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table id="multi-filter-select" class="display table table-striped table-hover">
-                      <thead>
-                        <tr>
-                          <th>Name</th>
-                          <th>Position</th>
-                          <th>Office</th>
-                          <th>Age</th>
-                          <th>Start date</th>
-                          <th>Salary</th>
-                        </tr>
-                      </thead>
-                      <tfoot>
-                        <tr>
-                          <th>Name</th>
-                          <th>Position</th>
-                          <th>Office</th>
-                          <th>Age</th>
-                          <th>Start date</th>
-                          <th>Salary</th>
-                        </tr>
-                      </tfoot>
-                      <tbody>
-                        <tr>
-                          <td>Tiger Nixon</td>
-                          <td>System Architect</td>
-                          <td>Edinburgh</td>
-                          <td>61</td>
-                          <td>2011/04/25</td>
-                          <td>$320,800</td>
-                        </tr>
-                        <tr>
-                          <td>Garrett Winters</td>
-                          <td>Accountant</td>
-                          <td>Tokyo</td>
-                          <td>63</td>
-                          <td>2011/07/25</td>
-                          <td>$170,750</td>
-                        </tr>
-                       
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
+    <x-model.dialog
+    title="Add Category"
+    btnLabel="Add Category"
+    >
+
+    </x-model.dialog>
+
+
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                {{-- <h4 class="card-title">Multi Filter Select</h4> --}}
             </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="multi-filter-select" class="display table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                            </tr>
+                            <tr>
+                                <td>Garrett Winters</td>
+                                <td>Accountant</td>
+                                <td>Tokyo</td>
+                                <td>63</td>
+                                <td>2011/07/25</td>
+                                <td>$170,750</td>
+                            </tr>
 
-
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
 @push('scripts')
-<script>
-      $("#multi-filter-select").DataTable({
-        stateSave: true,
-        lengthMenu: [5, 10, 25, 50, 150, 200 ],
-        pageLength: 5,
-        initComplete: function () {
-          this.api()
-            .columns()
-            .every(function () {
-              var column = this;
-              var select = $(
-                '<select class="form-select"><option value=""></option></select>'
-              )
-                .appendTo($(column.footer()).empty())
-                .on("change", function () {
-                  var val = $.fn.dataTable.util.escapeRegex($(this).val());
+    <script>
+        $("#multi-filter-select").DataTable({
+            stateSave: true,
+            lengthMenu: [5, 10, 25, 50, 150, 200],
+            pageLength: 5,
+            initComplete: function() {
+                this.api()
+                    .columns()
+                    .every(function() {
+                        var column = this;
+                        var select = $(
+                                '<select class="form-select"><option value=""></option></select>'
+                            )
+                            .appendTo($(column.footer()).empty())
+                            .on("change", function() {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
 
-                  column
-                    .search(val ? "^" + val + "$" : "", true, false)
-                    .draw();
-                });
+                                column
+                                    .search(val ? "^" + val + "$" : "", true, false)
+                                    .draw();
+                            });
 
-              column
-                .data()
-                .unique()
-                .sort()
-                .each(function (d, j) {
-                  select.append(
-                    '<option value="' + d + '">' + d + "</option>"
-                  );
-                });
-            });
-        },
-      });
-</script>
-
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function(d, j) {
+                                select.append(
+                                    '<option value="' + d + '">' + d + "</option>"
+                                );
+                            });
+                    });
+            },
+        });
+    </script>
 @endpush
-
---
+++ resources/views/components/model/dialog.blade.php
@@ -0,0 +1,30 @@
+    @props([
+        'title' => null,
+        'route' => 'admin.dashboard',
+        'target' => "model",
+        'btnLabel' => 'Submit'
+
+    ])
+
+    <!-- Modal -->
+    <div class="modal fade" id="{{ $target }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="model-title" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <form action="{{ route($route) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header bg-primary">
+                        <h1 class="modal-title fs-5" id="model-title text-white">{{ $title }}</h1>
+                        <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        {{ $slot }}
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="Submit" class="btn btn-primary">{{ $btnLabel }}</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
